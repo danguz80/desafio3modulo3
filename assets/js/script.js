aba9298de0bcce281fe99b5706f35c99ce325a67
Daniel Guzman Sagredo <danguz80@outlook.com>
2024-06-15 02:44:11 -0400

    Terminand3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
-/* Función original
+/* Función original 
 
 function pintar() {
     ele.style.backgroundColor = 'yellow'
 }
 const ele = document.getElementById("ele1")
 ele.addEventListener("click", pintar); 
-
 */
 
-/* Función anónima */
+
+/* Función anónima 
 const ele = document.getElementById("ele1")
 
 ele.addEventListener("click", function () {
     ele.style.backgroundColor = 'yellow'
 })
+*/
+
+/* Función con color "yellow" de argumento, al dejarlo sin argumento por defecto al hacer click el background se cambia a "green"  */
+
+function pintar(color = "green") {
+    ele.style.backgroundColor = color
+}
+const ele = document.getElementById("ele1")
+ele.addEventListener("click", function(){
+    pintar("yellow")
+}); 
